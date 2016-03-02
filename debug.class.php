cb5f873c7170694123f64ef0da60f3c4590f6a87
Edwin <edwin@tolima.be>
2016-03-02 17:19:29 +0100

    add debug.class

---
+++ debug.class.php
@@ -0,0 +1,56 @@
+<?php
+
+class Debug{
+    private static $DEBUG_PRINT_ON  = false;
+    private static $DEBUG_LEVEL = 1; //only print for level >= DEBUG_LEVEL.
+    
+    public static function getObjectContents($obj){
+        if (! isset($obj)) return "undefined";
+        if (is_null($obj)) return "null";
+        if (is_object($obj) || is_array($obj)){
+            if (is_object($obj)) $p = "->"; else $p = "[] = ";
+            $out = "<ul>";
+            foreach($obj as $k => $v)
+                $out .= "<li>" . $k . $p . self::getObjectContents($v) . "</li>";
+            return $out . "</ul>";
+        }
+        if ($obj === false)
+            return "false";
+        if ($obj === true)
+            return "true";        
+        return $obj;
+    }
+    
+    public static function eObjectContents($obj,$debugLevel=1){
+        if ($debugLevel < self::$DEBUG_LEVEL) return;
+        echo self::getObjectContents($obj);
+    }
+    
+    public static function e($str,$debugLevel=1,$newLine = true){
+        if ($debugLevel < self::$DEBUG_LEVEL) return;
+        if (self::$DEBUG_PRINT_ON){
+            echo $str;
+            if ($newLine) echo "<br/>";
+        }
+    }
+    
+    public static function g($str,$newLine = true){
+        if ($debugLevel < self::$DEBUG_LEVEL) return "";
+        if (self::$DEBUG_PRINT_ON){
+            if ($newLine) return $str . "<br/>";
+            return $str;
+        }
+        return "";
+    }
+    
+    public static function startDebugging($debugLevel=1){
+        if (is_numeric($debugLevel))
+            self::$DEBUG_LEVEL = $debugLevel;
+        self::$DEBUG_PRINT_ON = true;
+    }
+    
+    public static function stopDebugging(){
+        self::$DEBUG_PRINT_ON = false;
+    }
+}
+?>
